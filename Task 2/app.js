60a346330359751794d2791a28dad899514b891b
nada ayman <naymanallam@gmail.com>
2022-12-06 20:56:20 +0200

    Uploud

---
+++ Task 2/app.js
@@ -0,0 +1,44 @@
+const user = require("./modules/user.module")
+const yargs = require('yargs')
+yargs.command({
+    command:"add",
+    handler: function(argv){
+        user.add(argv)
+    }
+})
+yargs.command({
+    command:"showAll",
+    handler: function(argv){
+        user.showAll()
+    }
+})
+yargs.command({
+    command:"showSingle",
+    bulider:{
+        id:{demandOption:true}
+    },
+    handler: function(argv){
+        user.showSingle(argv.id)
+        
+    }
+})
+yargs.command({
+    command:"edit",
+    bulider:{
+        id:{demandOption:true},
+        status:{demandOption:true}
+    },
+    handler: function(argv){
+        user.edit(argv.id , argv.status)
+    }
+})
+yargs.command({
+    command:"del",
+    bulider:{
+        id:{demandOption:true},
+    },
+    handler: function(argv){
+        user.del(argv.id)
+    }
+})
+yargs.argv
